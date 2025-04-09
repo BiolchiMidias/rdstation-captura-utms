98139c60c08c8a42f5ef8c500ddbaba839cc90ef
Alexandre Biolchi <165388478+BiolchiMidias@users.noreply.github.com>
2025-04-09 14:19:59 -0300

    js-ocultar-campos-com-label.js
    Esse é o mais eficiente para formulários do RD Station, pois oculta o campo, o rótulo e o container.

---
+++ form/js-ocultar-campos-com-label.js
@@ -0,0 +1,21 @@
+function hidePreviousElementByName(name) {
+  var elements = document.querySelectorAll(name);
+  elements.forEach(function(element) {
+    var previousElement = element.previousElementSibling;
+    if (previousElement) {
+      previousElement.style.display = 'none';
+    }
+
+    var parentElement = element.parentElement;
+    if (parentElement) {
+      parentElement.style.display = 'none';
+    }
+  });
+}
+
+// Chame a função para cada campo UTM
+hidePreviousElementByName('[name="custom_fields[2704668]"]'); // utm_term
+hidePreviousElementByName('[name="custom_fields[2704671]"]'); // utm_content
+hidePreviousElementByName('[name="custom_fields[2704662]"]'); // utm_medium
+hidePreviousElementByName('[name="custom_fields[2704665]"]'); // utm_campaign
+hidePreviousElementByName('[name="custom_fields[2694055]"]'); // utm_source
